2baf38b8299bf515683a1ea7a41767c572ce89a3
bigonil <luca.bigoni@gmail.com>
2024-10-05 21:46:05 +0200

    Disable Bucket Policy

--- main.tf
+++ main.tf
@@ -12,9 +12,3 @@ resource "aws_s3_bucket_public_access_block" "lb_s3_bucket_public_access_block" 
   ignore_public_acls  = false
   restrict_public_buckets = false
 }
-
-# Optionally, add a bucket policy (for specific permissions)
-resource "aws_s3_bucket_policy" "lb_s3_bucket_policy" {
-  bucket = aws_s3_bucket.lb_s3_bucket.id
-  policy = jsonencode({ Statement = [{ Effect = "Allow", Principal = "*", Action = ["s3:GetObject"], Resource = ["arn:aws:s3:::lb-s3-bucket-acm/*"] }] })
-}
