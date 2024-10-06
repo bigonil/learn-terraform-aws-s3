b5fda37713afcec95bb3cbe10df90abccd1249cd
bigonil <luca.bigoni@gmail.com>
2024-10-06 15:48:39 +0200

    Delete variables.tf

--- variables.tf
+++
@@ -1,7 +0,0 @@
-# variables.tf
-
-variable "enable_versioning" {
-  description = "Enable versioning for the S3 bucket"
-  type        = bool
-  default     = false  # Set to false by default, can override when applying
-}
