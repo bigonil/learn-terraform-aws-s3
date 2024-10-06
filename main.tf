aaa9faf62012bb2622d43b3489eb1673bcea557c
bigonil <luca.bigoni@gmail.com>
2024-10-06 15:48:03 +0200

    Cooment a Policy command

--- main.tf
+++ main.tf
@@ -14,10 +14,10 @@ resource "aws_s3_bucket_versioning" "lb_s3_bucket_versioning" {
   }
 }
 
-resource "aws_s3_bucket_acl" "lb_s3_bucket_acl" {
-  bucket = aws_s3_bucket.lb_s3_bucket.id
-  acl    = "public-read"
-  } 
+#resource "aws_s3_bucket_acl" "lb_s3_bucket_acl" {
+#  bucket = aws_s3_bucket.lb_s3_bucket.id
+#  acl    = "public-read"
+#  } 
 
 # Separate the website configuration for the S3 bucket
 resource "aws_s3_bucket_website_configuration" "lb_s3_bucket_website" {
@@ -45,12 +45,27 @@ resource "aws_s3_bucket_public_access_block" "lb_s3_bucket_public_access_block" 
 
 # Configure the bucket policy to allow public access to the website content
 
+resource "aws_s3_bucket_policy" "lb_s3_static_website_policy" {
+  bucket = aws_s3_bucket.lb_s3_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = "s3:GetObject",
+        Resource  = "${aws_s3_bucket.lb_s3_bucket.arn}/*"
+      }
+    ]
+  })
+}
+
 # Upload the index.html file to the S3 bucket
 resource "aws_s3_object" "index" {
   bucket = aws_s3_bucket.lb_s3_bucket.bucket
   key    = "index.html"
   source = "index.html"  # Provide the path to your local index.html file
-  acl    = "public-read"
 }
 
 # Upload the error.html file to the S3 bucket
@@ -58,8 +73,7 @@ resource "aws_s3_object" "error" {
   bucket = aws_s3_bucket.lb_s3_bucket.bucket
   key    = "error.html"
   source = "error.html"  # Provide the path to your local error.html file
-  acl    = "public-read"
-}
+ }
 
 output "website_url" {
   value = aws_s3_bucket_website_configuration.lb_s3_bucket_website.website_endpoint
